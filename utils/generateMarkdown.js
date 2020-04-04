b04ee5ce3b98bd93f23d93aea8d162b210a0b903
Albert Cervantes <cervanalbert@gmail.com>
2020-04-03 22:03:15 -0700

    added test markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,8 @@
-// TODO: Return markdown string for README file given a data object.
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+module.exports = function generateMarkdown(data) {
+  const { avatar_url, login } = data; // use object destructuring
 
+  return `# Profile!
+![${login} avatar](${avatar_url})
+This is the end of ReadMe.....
 `;
 }
-
-module.exports = generateMarkdown;
