7e7406fb6e859aead8ef55bb678febf9e1d832ed
Albert Cervantes <cervanalbert@gmail.com>
2020-04-03 22:02:00 -0700

    finished getUser function

--- utils/api.js
+++ utils/api.js
@@ -1,13 +1,10 @@
-// TODO: import axios module
-
-// TODO: use dotenv module to get environmental variables if necessary
-
-// TODO: do not make a commit with GitHub api key or access token in any file. (Only applies if your app uses access tokens or api keys.)
-
+const axios = require("axios");
 const api = {
-  // TODO: Return promise for GitHub api response to get user data.
-  // (Hint: Use axios to send a get request and return the promise created by calling axios.get())
-  getUser(username) {}
+  getUser(username) {
+    const url = `https://api.github.com/users/${username}`;
+    console.log("Searching...\n");
+    return axios.get(url);
+  }
 };
 
 module.exports = api;
